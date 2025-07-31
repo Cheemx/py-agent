334850c046d6f4703840409a6060bafcf7d661f7
Cheemx <chinmaymahajan999@gmail.com>
2025-07-31 04:47:22 +0000

    ref: gemini app init

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from py-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai 
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    res = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+    print(res.text)
+    # print(f"Prompt tokens:{res}")
+    print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
