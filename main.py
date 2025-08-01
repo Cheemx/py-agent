7524cade32181f19079bad5d67a7ad06af59174f
Cheemx <chinmaymahajan999@gmail.com>
2025-08-01 14:49:14 +0000

    chapter 1 completed!

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai 
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    flag = sys.argv[2]
+    if user_prompt == "":
+        sys.exit("User Prompt is Required!")
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     res = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=messages
     )
     print(res.text)
     # print(f"Prompt tokens:{res}")
-    print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
+    if flag:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
